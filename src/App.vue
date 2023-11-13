1430dfc2231c66c4aa6ad43a3908cfcef8b5ccf9
libin ann babu <libin.babu@altd.in>
2023-11-13 15:37:27 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <SideBar @selectOption="onSelectOption" />
+    <v-main>
+      <ImageGallery :isWithColor="isWithColor" />
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import SideBar from './components/SideBar.vue';
+import ImageGallery from './components/ImageGallery.vue';
+import { ref } from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+const isWithColor = ref(true);
+const onSelectOption = (isColor) => {
+  isWithColor.value = isColor;
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-navigation-drawer expand-on-hover rail>
+        <v-list>
+            <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="Sandra Adams"
+                subtitle="sandra_a88@gmailcom"></v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list density="compact" nav>
+            <v-list-item prepend-icon="mdi-image" title="Color Gallery" @click="selectOption(true)"
+                value="color"></v-list-item>
+            <v-list-item prepend-icon="mdi-dialpad" title="Grayspace Gallery" @click="selectOption(false)"
+                value="color"></v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue';
+const emits = defineEmits(['selectOption'])
+const selectOption = (isColor) => {
+    emits("selectOption", isColor)
+}
+</script>
